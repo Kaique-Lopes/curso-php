42dd7a4ab8c05ecb67069dbfe3d52bf5b24ac942
Kaique Lopes <kaique.lopes3271@gmail.com>
2021-03-25 10:27:46 -0300

    exercise

---
+++ exercicio.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Syne+Mono&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <title>Exercicio</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso Kaique - PHP</h1>
+        <h2>Visualização do exercicio</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class= "verde">Sem formatacao</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+            
+        </div>
+    </main>
+    <footer class="rodape">
+        Kaique Lopes <?= date ('Y'); ?>
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&family=Syne+Mono&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <title>Curso-Php</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso Kaique - PHP</h1>
+        <h2>Indice Exercicios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li><a href="exercicio.php">Exercícío A</a></li>
+                        <li><a href="#">Exercícío B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+                <div class="modulo red">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercícío A</a></li>
+                        <li><a href="#">Exercícío B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+                <div class="modulo amarelo">
+                    <h3>Módulo 03</h3>
+                    <ul>
+                        <li><a href="#">Exercícío A</a></li>
+                        <li><a href="#">Exercícío B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+                <div class="modulo blue">
+                    <h3>Módulo 04</h3>
+                    <ul>
+                        <li><a href="#">Exercícío A</a></li>
+                        <li><a href="#">Exercícío B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+                <div class="modulo laranja">
+                    <h3>Módulo 05</h3>
+                    <ul>
+                        <li><a href="#">Exercícío A</a></li>
+                        <li><a href="#">Exercícío B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+                <div class="modulo rosa">
+                    <h3>Módulo 06</h3>
+                    <ul>
+                        <li><a href="#">Exercícío A</a></li>
+                        <li><a href="#">Exercícío B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+                <div class="modulo roxo">
+                    <h3>Módulo 07</h3>
+                    <ul>
+                        <li><a href="#">Exercícío A</a></li>
+                        <li><a href="#">Exercícío B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+                <div class="modulo aqua">
+                    <h3>Módulo 08</h3>
+                    <ul>
+                        <li><a href="#">Exercícío A</a></li>
+                        <li><a href="#">Exercícío B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+                <div class="modulo marrom">
+                    <h3>Módulo 09</h3>
+                    <ul>
+                        <li><a href="#">Exercícío A</a></li>
+                        <li><a href="#">Exercícío B</a></li>
+                        <li><a href="#">Exercício C</a></li>
+                        <li><a href="#">Exercício D</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        Kaique Lopes <?= date ('Y'); ?>
+    </footer>
+</body>
+</html>
